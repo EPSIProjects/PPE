67b0b384ee41066ff9d289b35aa51b779baf5695
Beetlejuice <marius.40.lemonnier@gmail.com>
2016-05-19 17:34:45 +0200

    modifs +

--- Leger/Site/a-propos/index.php
+++ Leger/Site/a-propos/index.php
@@ -15,7 +15,7 @@
 			<p id="aboutPara">
 				Para-Vent est une entreprise fondée en 2004 à Bordeaux par deux amis, Vincent Castel et Jessie James. Après deux ans de collaboration Jessie quitte l'entreprise pour se réorienter. A ses débuts, la société proposait des tours de parapente en tandem au-dessus de la dune du Pilat. Un an après le départ de Jessie, l'entreprise ouvrit une nouvelle enseigne à Tarbes, dans les Hautes-Pyrénées. Devant le succès de activité, Para-Vent a ouvert plusieurs autres établissements à travers toute la France.
 			</p>
-			<img src="../images/Lac_d_Oredon.jpg" alt="Lac d'Orédon" width="939" height="412">
+			<img class="img-circle center-block" src="../images/Lac_d_Oredon.jpg" alt="Lac d'Orédon" width="1008" height="520">
 			<br>
 			<p id="aboutPara">
 				Aujourd'hui, Para-Vent propose plusieurs services, comme des sauts en parachute, en parapente et en wingsuit. De débutant à expert, tout le monde peut y trouver son compte. La société propose même des formations pour ceux qui veulent être autonomes.

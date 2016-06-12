d63786feaee4bc71d0858fb1a5c19fc0e5b36e62
nicopeva <nicopaucant@hotmail.fr>
2016-06-12 14:33:03 +0200

    equipement (re-executer le script sql pour la bdd)

--- Leger/Site/equipements/index.php
+++ Leger/Site/equipements/index.php
@@ -1,4 +1,3 @@
-
 <?php $title="Équipements"; ?>
 <?php include '../header.php'; ?>
 <?php include '../connexion.php'; ?>
@@ -6,24 +5,59 @@
             document.getElementById("equipements").className = "active";
       </script>
 
+    <h1>Equipements</h1>
 <?php
-        $query = 'select * from article';
+        $query = 'select * from article order by libelle_article asc';
         $result = mysqli_query($bdd, $query);
 
+        while($donnees = mysqli_fetch_array($result, MYSQLI_ASSOC))
+        {
+            $tableau[] = $donnees;
+        }
+        echo'<table class="tabprod"><tr>';
+        foreach ($tableau as $tab){
+            echo '<td style="border: solid black 1px; font-size: 10px;">'.$tab['libelle_article'].'</td>';
+        }
+        echo '</tr>';
 
-        echo '<table style="margin: auto; text-align: center; border: solid black 1px;">
-            <tr>
-                <td style="font-weight: bold">Équipement</td>
-            </tr>';
-            while ($donnees = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                echo'<tr>
-                    <td>'.$donnees ['libelle_article'].'<img src="../images/suit.jpg" height="75" widht="75"/></td>
-                </tr>';
-            }
-        echo'</table>';
+        foreach ($tableau as $tab){
+            $nomImg = $tab['libelle_article'];
+            echo '<td style="border: solid black 1px;"><img style="width: 30px" src="../images/tabProd/'.$nomImg.'.jpg"></td>';
+        }
+        echo '</tr>';
+
+        foreach ($tableau as $tab){
+            $nomImg = $tab['prix_article'];
+            echo '<td style="border: solid black 1px;">'.$tab['prix_article'].' €</td>';
+        }
+
+        echo'</tr></table>';
 ?>
+    <br>
+    <h2>Réserver un équipement</h2>
+    <?php
+        echo '<form method="post" action="reservation.php">
+                <select name="combobox">';
+            foreach ($tableau as $tab){
+                echo '<option>'.$tab['libelle_article'].'</option>';
+            }
+            echo'</select>';
+    ?>
 
+    <br>
 
+    <label for="nom">Nom et prénom :</label>
+        <input type="text" class="form-control" id="nom" name="nom">
 
+    <label for="email">Email :</label>
+        <input type="email" class="form-control" id="email" name="mail">
+
+    <label for="telephone">Téléphone :</label>
+        <input type="tel" class="form-control" id="tel" name="tel">
+
+    <br><br>
+
+        <button type="submit" class="btn btn-info">Réserver</button>
+    </form>
 
 <?php include '../footer.php'; ?>
